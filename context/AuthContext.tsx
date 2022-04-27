82a4cea3f6bb9eea3e0b15c018211bee2e75661e
marcelo-gomj <marcelomgj005@gmail.com>
2022-04-27 17:02:14 -0300

    update: salva o token e refreshToken no cookie

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,8 +1,8 @@
-import { createContext, ReactNode } from 'react';
-import { api } from '../services/api';
-import { useState } from 'react';
+import { createContext, ReactNode, useEffect, useState } from 'react';
 import Router from 'next/router';
 
+import { api } from '../services/api';
+import { parseCookies, setCookie } from 'nookies';
 
 type SignInCredentails = {
     email: string;
@@ -31,6 +31,17 @@ export function AuthProvider({children}: AuthProviderProps){
     const [user, setUser] = useState<User>();
     const isAuthenticated = !!user;
 
+    useEffect(()=> {
+        const { "jwt-auth.token" : token } = parseCookies(); 
+
+        if(token){
+            api.get('/me').then( response => {
+                const { email, permissions, roles } = response.data;
+                setUser({ email, permissions, roles })
+            })
+        }
+    }, [])
+
     async function signIn({email, password}: SignInCredentails){
         try{
             const response = await api.post('sessions', {
@@ -39,8 +50,20 @@ export function AuthProvider({children}: AuthProviderProps){
             })
     
             const { token, refreshToken, roles, permissions } = response.data;
+            
+            setCookie(undefined, 'jwt-auth.token', token, {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: '/'
+            });
 
+            setCookie(undefined,  'jwt-auth.refreshToken', refreshToken, {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: '/'
+            });
+            
             setUser({email, roles, permissions})
+
+            api.defaults.headers["Authorization"] = `Bearer ${token}`
 
             Router.push('/dashboard');
 
--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,27 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import { parseCookies } from 'nookies';
+
+let cookies = parseCookies();
 
 export const api =  axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'http://localhost:3333', 
+    headers: {
+        Authorization: `Bearer ${cookies['jwt-auth.token']}`
+    }
 });
+
+api.interceptors.response.use(response => {
+    return response
+}, (error: AxiosError) => {
+    if(error.response.status === 401){
+        if(error.response.data?.code === 'token.expired'){
+            cookies = parseCookies();
+
+            const { 'jwt-auth.refreshToken': refreshToken } = cookies;
+            
+
+        }else{
+
+        }
+    }
+})
